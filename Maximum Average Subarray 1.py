5ad96b97849d72bf27ca86fc8fe6311000f12409
codingmatemoon <abc9529@naver.com>
2023-10-06 09:12:01 +0900

    [leetcode] Maximum Average Subarray 1 (Cumulative Sum)

--- Maximum Average Subarray 1.py
+++ Maximum Average Subarray 1.py
@@ -9,6 +9,23 @@ class Solution(object):
 
     def findMaxAverage(self, nums, k):
 
+        # Cumulative Sum
+        sum = [0] * len(nums)
+        sum[0] = nums[0]
+        print(f"{nums[0]} : {sum[0]}")
+
+        for i in range(1, len(nums)):
+            sum[i] = sum[i - 1] + nums[i]
+        result = sum[k - 1] * 1.0 / k
+        print(f"result : {result}")
+
+        for i in range(k, len(nums)):
+            result = max(result, (sum[i] - sum[i - k]) * 1.0 / k)
+
+        return result
+
+    def findMaxAverage_1(self, nums, k):
+
         # right 커서를 1씩 증가시키면서 sum += num[right], length가 k를 넘을 경우 left + 1, sum -= num[left] 수행. max(answer, sum)
         # sum / length -> 평균 구하기
         left = length = 0
