c37e45fc7867f7278434f87cb2197bc8f112efd3
codingmatemoon <janghyun2478@gmail.com>
2023-10-10 00:15:16 +0900

    [leetcode] Max Consecutive Ones 3 [완료]

--- Max Consecutive Ones 3.py
+++ Max Consecutive Ones 3.py
@@ -16,6 +16,21 @@ class Solution:
     """
 
     def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            # If we included a zero in the window we reduce the value of k.
+            # Since k is the maximum zeros allowed in a window.
+            k -= 1 - nums[right]
+            print(f"right : {right} / k : {k} / nums[right] : {nums[right]}")
+            # A negative k denotes we have consumed all allowed flips and window has
+            # more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if k < 0:
+                # If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left]
+                left += 1
+        return right - left + 1
+
+    def longestOnes_1(self, nums: List[int], k: int) -> int:
         count = length = result = 0
         zero_list = []
         start_index = 0
