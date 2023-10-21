e83130a59f218920361d787824bce1dce9453fcc
codingmatemoon <janghyun2478@gmail.com>
2023-10-21 10:59:41 +0900

    [leetcode] K Radius Subaary Averages [진행]

---
+++ K Radius Subarray Averages.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    """
+    n == nums.length
+    1 <= n <= 105
+    0 <= nums[i], k <= 105
+
+    Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
+Output: [-1,-1,-1,5,4,4,-1,-1,-1]
+Explanation:
+    - avg[0], avg[1], and avg[2] are -1 because there are less than k elements before each index.
+    - The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 + 1 + 8 + 5 = 37.
+      Using integer division, avg[3] = 37 / 7 = 5.
+    - For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7 = 4.
+    - For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7 = 4.
+    - avg[6], avg[7], and avg[8] are -1 because there are less than k elements after each index.
+        """
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+
+        for i in range(len(nums)):
+
+
+if __name__ == '__main__':
+    print(Solution().getAverages([7,4,3,9,1,8,5,2,6]), 3)
