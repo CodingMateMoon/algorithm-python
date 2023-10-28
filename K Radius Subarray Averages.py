16b76721574fb06525230768b515035caf3d5d23
codingmatemoon <janghyun2478@gmail.com>
2023-10-28 10:31:59 +0900

    [leetcode] K Radius Subarray Averages [보완1]

--- K Radius Subarray Averages.py
+++ K Radius Subarray Averages.py
@@ -23,7 +23,36 @@ Explanation:
     i = 4 -> total = total - left[0] + right[7]
     i = k -> total = total - left[i - k - 1] + right[i + k]
         """
+
     def getAverages(self, nums: List[int], k: int) -> List[int]:
+        # When a single element is considered then its average will be the number itself only.
+        if k == 0:
+            return nums
+
+        n = len(nums)
+        averages = [-1] * n
+
+        # Any index will not have 'k' elements in it's left and right.
+        if 2 * k + 1 > n:
+            return averages
+
+        # Generate 'prefix' array for 'nums'.
+        # 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+        # We iterate only on those indices which have atleast 'k' elements in their left and right.
+        # i.e. indices from 'k' to 'n - k'
+        for i in range(k, n - k):
+            leftBound, rightBound = i - k, i + k
+            subArraySum = prefix[rightBound + 1] - prefix[leftBound]
+            average = subArraySum // (2 * k + 1)
+            averages[i] = average
+
+        return averages
+
+    def getAverages_1(self, nums: List[int], k: int) -> List[int]:
 
         length = len(nums)
         answer = [0] * length
