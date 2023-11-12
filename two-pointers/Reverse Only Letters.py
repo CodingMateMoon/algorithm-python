498d470dcba61a861798c4a0e1bc2ff8ef46d100
codingmatemoon <janghyun2478@gmail.com>
2023-11-12 17:39:57 +0900

    [leetcode] Reverse Only Letters [보완2]

--- two-pointers/Reverse Only Letters.py
+++ two-pointers/Reverse Only Letters.py
@@ -5,6 +5,19 @@ class Solution:
     s does not contain '\"' or '\\'.
     """
     def reverseOnlyLetters(self, s: str) -> str:
+        ans = []
+        j = len(ans) - 1
+        for i, x in enumerate(s):
+            if x.isalpha():
+                while not s[j].isalpha():
+                    j -= 1
+                ans.append(s[j])
+                j -= 1
+            else:
+                ans.append(x)
+
+        return "".join(ans)
+    def reverseOnlyLetters_2(self, s: str) -> str:
         letters = [c for c in s if c.isalpha()]
         ans = []
         for c in s:
