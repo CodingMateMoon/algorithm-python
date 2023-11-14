7b8503c6539cda024de8d46fc88d12707fd9b84d
codingmatemoon <janghyun2478@gmail.com>
2023-11-14 09:40:18 +0900

    [leetcode] Move Zeroes [진행2]

--- two-pointers/Move Zeros.py
+++ two-pointers/Move Zeros.py
@@ -2,13 +2,18 @@ from typing import List
 
 
 class Solution:
+    """
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+    0을 제외한 나머지 숫자에 대해 리스트를 만들고 0의 개수를 센 뒤에 뒤에 덧붙입니다
+    """
     def moveZeroes(self, nums: List[int]) -> List[int]:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums = [1, 3, 12, 0, 0]
         return nums
 
 def test_moveZeroes():
     solution = Solution()
     assert solution.moveZeroes([0, 1, 0, 3, 12]) == [1, 3, 12, 0, 0]
+    assert solution.moveZeroes([0]) == [0]
