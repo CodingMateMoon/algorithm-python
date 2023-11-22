50861b663a2d57ae29b32c0dac3cb25e775f8e88
codingmatemoon <janghyun2478@gmail.com>
2023-11-22 09:54:00 +0900

    [leetcode] Reverse Prefix of Word [진행2]

--- two-pointers/Reverse Prefix of Word.py
+++ two-pointers/Reverse Prefix of Word.py
@@ -2,7 +2,13 @@ class Solution:
     # https://leetcode.com/problems/reverse-prefix-of-word/
     def reversePrefix(self, word: str, ch: str) -> str:
 
+        for i in range (len(word)):
+            if word[i] == ch:
+                for j in (i, 0, -1):
+                    print(f"[{j}]")
+
         return "dcbaefd"
 def test_reversePrefix():
     solution = Solution()
     assert solution.reversePrefix("abcdefd", "d") == "dcbaefd"
+    assert solution.reversePrefix("abcd", "z") == "abcd"
