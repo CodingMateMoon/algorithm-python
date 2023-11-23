29a6f1f51c911ea5ac7a24b3c5d5d8eaf49fbc20
codingmatemoon <janghyun2478@gmail.com>
2023-11-23 09:15:00 +0900

    [leetcode] Reverse Prefix of Word [진행3]

--- two-pointers/Reverse Prefix of Word.py
+++ two-pointers/Reverse Prefix of Word.py
@@ -1,11 +1,20 @@
 class Solution:
     # https://leetcode.com/problems/reverse-prefix-of-word/
+    """
+    시작 인덱스 left부터 abcdefd 기준 d와 일치하는 인덱스까지 스택에 데이터를 넣고 다 넣은 뒤 pop
+    """
     def reversePrefix(self, word: str, ch: str) -> str:
+
+        left = 0
 
         for i in range (len(word)):
             if word[i] == ch:
-                for j in (i, 0, -1):
-                    print(f"[{j}]")
+                temp = []
+                for j in range(left, i):
+                    temp.append(word[j])
+
+
+
 
         return "dcbaefd"
 def test_reversePrefix():
