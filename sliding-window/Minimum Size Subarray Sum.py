fc839bf294fc76acedf9316384656ddff9a27e9a
codingmatemoon <janghyun2478@gmail.com>
2023-11-30 12:54:20 +0900

    [leetcode] Minimum Size Subarray Sum [진행3]

--- sliding-window/Minimum Size Subarray Sum.py
+++ sliding-window/Minimum Size Subarray Sum.py
@@ -2,8 +2,23 @@
 class Solution:
     """
     부분 집합의 합 중 target보다 크거나 같은 최소 길이의 부분 집합을 구합니다
+    앞에서 부분 집합의 합 중 target이상인 값이 나와도 더 길이가 작은 부분 집합이 뒤에 있을 수 있습니다 ex) 2 + 3 + 1 + 2 < 4 + 3
     """
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        sum = left = length = 0
+
+        for i in range (len(nums)):
+            if sum < target:
+                sum += nums[i]
+                length += 1
+                continue
+            sum -= nums[left]
+            left += 1
+            sum += nums[i]
+
+
+
 
         return 2
 def test_minSubArrayLen():
