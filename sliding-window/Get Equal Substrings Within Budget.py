086973bb3c3c2560b1603744510d5b79a10704ff
codingmatemoon <janghyun2478@gmail.com>
2023-12-09 12:46:23 +0900

    [leetcode] Get Equal Substrings Within Budget [진행2]

--- sliding-window/Get Equal Substrings Within Budget.py
+++ sliding-window/Get Equal Substrings Within Budget.py
@@ -1,4 +1,11 @@
 class Solution:
+    # https://leetcode.com/problems/get-equal-substrings-within-budget/description/
+    """
+    1 <= s.length <= 105
+    t.length == s.length
+    0 <= maxCost <= 106
+    s and t consist of only lowercase English letters.
+    """
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
 def test_equalSubstring():
     solution = Solution()
