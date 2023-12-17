214c5e13077a88f74383d1189bf8c9b58905e4f4
codingmatemoon <janghyun2478@gmail.com>
2023-12-17 23:48:04 +0900

    [leetcode] Find the Highest Altitude (진행1)

---
+++ prefix-sum/Find the Highest Altitude.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        return 0
+
+
+def test_largestAltitude():
+    solution = Solution()
+    assert solution.largestAltitude([-5, 1, 5, 0, -7]) == 1
