390ac80facfaf919965e10dfae55f46c25243e70
codingmatemoon <janghyun2478@gmail.com>
2023-12-21 13:56:47 +0900

    [leetcode] Find the Highest Altitude [진행1]

--- prefix-sum/Find the Highest Altitude.py
+++ prefix-sum/Find the Highest Altitude.py
@@ -1,9 +1,27 @@
 from typing import List
 
 
+#https://leetcode.com/problems/find-the-highest-altitude/description/
+"""
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+"""
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        return 0
+
+        sum = highest_altitude = 0
+
+        for altitude in gain:
+            sum += altitude
+            highest_altitude = max(highest_altitude, sum)
+
+        return highest_altitude
 
 
 def test_largestAltitude():
