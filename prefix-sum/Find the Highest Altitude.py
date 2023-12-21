f9d737590b35c6fa43155522d0f675091499eb45
codingmatemoon <janghyun2478@gmail.com>
2023-12-22 08:57:56 +0900

    [leetcode] Find the Highest Altitude [보완]

--- prefix-sum/Find the Highest Altitude.py
+++ prefix-sum/Find the Highest Altitude.py
@@ -13,7 +13,21 @@ Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 
 """
 class Solution:
+
     def largestAltitude(self, gain: List[int]) -> int:
+        current_altitude = 0
+        # Highest altitude currently is 0.
+        highest_point = current_altitude
+
+        for altitude_gain in gain:
+            # Adding the gain in altitude to the current altitude.
+            current_altitude += altitude_gain
+            # Update the highest altitude.
+            highest_point = max(highest_point, current_altitude)
+
+        return highest_point
+
+    def largestAltitude_1(self, gain: List[int]) -> int:
 
         sum = highest_altitude = 0
 
@@ -27,3 +41,4 @@ class Solution:
 def test_largestAltitude():
     solution = Solution()
     assert solution.largestAltitude([-5, 1, 5, 0, -7]) == 1
+    assert solution.largestAltitude([-4, -3, -2, -1, 4, 3,2]) == 0
