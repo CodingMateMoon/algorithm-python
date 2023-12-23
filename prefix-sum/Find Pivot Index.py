8380a52ed88f9e3d02d12b072aaecc20bb909202
codingmatemoon <janghyun2478@gmail.com>
2023-12-23 10:23:00 +0900

    [leetcode] Find Pivot Index (진행1)

---
+++ prefix-sum/Find Pivot Index.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+def test_pivotIndex():
+    solution = Solution()
+    assert solution.pivotIndex([1,7,3,6,5,6]) == 3
