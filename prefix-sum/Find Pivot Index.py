c44a277fd0bc57829beb78f78a235e0e24d10447
codingmatemoon <janghyun2478@gmail.com>
2023-12-24 13:46:30 +0900

    [leetcode] Find Pivot Index (진행1)

--- prefix-sum/Find Pivot Index.py
+++ prefix-sum/Find Pivot Index.py
@@ -1,9 +1,13 @@
 from typing import List
 
 
+# https://leetcode.com/problems/find-pivot-index/description/
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
 
+
 def test_pivotIndex():
     solution = Solution()
-    assert solution.pivotIndex([1,7,3,6,5,6]) == 3
+    assert solution.pivotIndex([1, 7, 3, 6, 5, 6]) == 3
+    assert solution.pivotIndex([1, 2, 3]) == -1
+    assert solution.pivotIndex([2, 1, -1]) == 0
