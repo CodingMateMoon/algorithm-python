63b57c42662c17e22ec78d370582f813b7840636
codingmatemoon <janghyun2478@gmail.com>
2023-12-28 09:15:11 +0900

    [leetcode] Find Pivot Index (완료)

--- prefix-sum/Find Pivot Index.py
+++ prefix-sum/Find Pivot Index.py
@@ -8,20 +8,16 @@ class Solution:
         for num in nums:
             right_sum += num
         right_sum -= nums[0]
-        for pivot_index in range(len(nums) - 1):
-            print(f"left : {left_sum} / right : {right_sum}")
+        for i in range(len(nums) - 1):
+            print(f"[pivot : {i}] left : {left_sum} / right : {right_sum}")
             if left_sum == right_sum:
-                return pivot_index
-
-            left_sum += nums[pivot_index]
-            right_sum -= nums[pivot_index + 1]
+                return i
+            left_sum += nums[i]
+            right_sum -= nums[i + 1]
+            pivot_index += 1
         if left_sum == right_sum:
             return pivot_index
         return -1
-
-
-
-
 
 
 def test_pivotIndex():
@@ -32,9 +28,10 @@ def test_pivotIndex():
     (2) leftmost index = -1
     (3) leftmost index = 0
     0 = 1 - 1 = 0
-    
-    """
+    (4) expected : 5, actual 4
+    -1, -1  
     assert solution.pivotIndex([1, 7, 3, 6, 5, 6]) == 3
     assert solution.pivotIndex([1, 2, 3]) == -1
     assert solution.pivotIndex([2, 1, -1]) == 0
-    assert solution.pivotIndex([-1,-1,0,1,1,0]) == 5
+    """
+    assert solution.pivotIndex([-1, -1, 0, 1, 1, 0]) == 5
