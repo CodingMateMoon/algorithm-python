3f03fac7f14360b2f455b1fbc6b9532b61512f2d
codingmatemoon <janghyun2478@gmail.com>
2023-12-31 11:26:04 +0900

    [leetcode] Range Sum Query - Immutable (완료)

--- prefix-sum/Range Sum Query - Immutable.py
+++ prefix-sum/Range Sum Query - Immutable.py
@@ -4,15 +4,19 @@ from typing import List
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        print("test")
-
+        self.nums = nums
     def sumRange(self, left: int, right: int) -> int:
-        return 1
+        sum = 0
+        for i in range(left, right + 1):
+           sum += self.nums[i]
+        return sum
 
 
 def test_sumRange():
     obj = NumArray([-2, 0, 3, -5, 2, -1])
     assert obj.sumRange(0, 2) == 1
+    assert obj.sumRange(2, 5) == -1
+    assert obj.sumRange(0, 5) == -3
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
