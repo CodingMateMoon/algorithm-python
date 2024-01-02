eb6edca2f1ed06798c3686ee5f544a67b47c7d56
codingmatemoon <janghyun2478@gmail.com>
2024-01-03 08:28:36 +0900

    [leetcode] Two sum [진행2]

--- hashing/Two sum.py
+++ hashing/Two sum.py
@@ -5,3 +5,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
 def test_twoSum():
+    solution = Solution()
+    assert solution.twoSum([2, 7, 11, 15]) == 9
+    assert solution.twoSum([3, 2, 4]) == 6
+    assert solution.twoSum([3, 3]) == 6
