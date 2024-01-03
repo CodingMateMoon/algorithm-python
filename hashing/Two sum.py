335301e1db06b831f949d58c58056d1cf32e2bbf
codingmatemoon <janghyun2478@gmail.com>
2024-01-04 08:26:51 +0900

    [leetcode] Two sum [진행3]

--- hashing/Two sum.py
+++ hashing/Two sum.py
@@ -1,11 +1,13 @@
 from typing import List
 
+
 # https://leetcode.com/problems/two-sum/
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+
 def test_twoSum():
     solution = Solution()
-    assert solution.twoSum([2, 7, 11, 15]) == 9
-    assert solution.twoSum([3, 2, 4]) == 6
-    assert solution.twoSum([3, 3]) == 6
+    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert solution.twoSum([3, 2, 4], 6) == [1, 2]
+    assert solution.twoSum([3, 3], 6) == [0, 1]
