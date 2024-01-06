27249f90fea675834f923bf4cf2f4d87ea09a94d
codingmatemoon <janghyun2478@gmail.com>
2024-01-06 09:54:15 +0900

    [leetcode] Two sum [진행5]

--- hashing/Two sum.py
+++ hashing/Two sum.py
@@ -17,6 +17,10 @@ class Solution:
 
 def test_twoSum():
     solution = Solution()
+    """
+    연속된 합뿐만 아니라 0, 2 등 순서 및 위치 상관없이 두 인덱스 조합으로 가능, 0 ~ i - 1까지 각 요소에 대해 다른 하나와 sum을 구했을 때 
+    target이 나오는 경우 구하기
+    """
     assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
     assert solution.twoSum([3, 2, 4], 6) == [1, 2]
     assert solution.twoSum([3, 3], 6) == [0, 1]
