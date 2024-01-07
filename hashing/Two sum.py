c8976cf17f5079bdd346567cff3006166afc16be
codingmatemoon <janghyun2478@gmail.com>
2024-01-08 08:36:21 +0900

    [leetcode] Two sum [완료]

--- hashing/Two sum.py
+++ hashing/Two sum.py
@@ -8,6 +8,9 @@ class Solution:
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return [-1]
                 
 
 
