8262b7b0f66f2500b7164610852f7af6cadda06e
codingmatemoon <janghyun2478@gmail.com>
2024-01-13 10:28:24 +0900

    [leetcode] Check if the Sentence is Pangram [진행1]

---
+++ hashing/Check if the Sentence is Pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        return True
+
+def test_checkIfPangram():
+    solution = Solution()
+    solution.checkIfPangram("thequickbrownfoxjumpsoverthelazydog") == True
