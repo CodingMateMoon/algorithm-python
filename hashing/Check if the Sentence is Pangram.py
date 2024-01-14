8cc952e454722f834232208bce728a0f21e17532
codingmatemoon <janghyun2478@gmail.com>
2024-01-14 12:03:25 +0900

    [leetcode] Check if the Sentence is Pangram [진행2]

--- hashing/Check if the Sentence is Pangram.py
+++ hashing/Check if the Sentence is Pangram.py
@@ -1,7 +1,10 @@
 class Solution:
+
+    # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4601/
     def checkIfPangram(self, sentence: str) -> bool:
         return True
 
 def test_checkIfPangram():
     solution = Solution()
     solution.checkIfPangram("thequickbrownfoxjumpsoverthelazydog") == True
+    solution.checkIfPangram("leetcode") == False
