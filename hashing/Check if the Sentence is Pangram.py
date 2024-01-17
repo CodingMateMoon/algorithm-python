35c3b1ab7116f34ce4d3a8c2a8a9051d4a06eaa2
codingmatemoon <janghyun2478@gmail.com>
2024-01-17 10:23:01 +0900

    [leetcode] Check if the Sentence is Pangram [진행5]

--- hashing/Check if the Sentence is Pangram.py
+++ hashing/Check if the Sentence is Pangram.py
@@ -3,10 +3,16 @@ class Solution:
     # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4601/
     # a sentence where every letter of the English alphabet appears at least once.
     def checkIfPangram(self, sentence: str) -> bool:
-        isUsed = [0] * len(ord('z') - ord('a') + 1)
-        
-        for char in range(ord('a'), ord('z') + 1):
-            print(chr(char))
+        isUsed = [0] * (ord('z') - ord('a') + 1)
+
+        for i in range(len(sentence)):
+            print(ord(sentence[i]))
+            #isUsed[ord(sentence[i])] = True
+
+        for i in range(isUsed):
+            if isUsed[i] == False:
+                return False
+
         return True
 
 def test_checkIfPangram():
