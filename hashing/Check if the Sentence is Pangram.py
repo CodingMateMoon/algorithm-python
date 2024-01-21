46e354d8766cf196af6ae9be0a1b8008eee08997
codingmatemoon <janghyun2478@gmail.com>
2024-01-22 08:35:24 +0900

    [leetcode] Check if the Sentence is Pangram [보완4]

--- hashing/Check if the Sentence is Pangram.py
+++ hashing/Check if the Sentence is Pangram.py
@@ -3,6 +3,24 @@ class Solution:
     # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4601/
     # a sentence where every letter of the English alphabet appears at least once.
     def checkIfPangram(self, sentence: str) -> bool:
+        # Initialize seen = 0 since we start with no letter.
+        seen = 0
+
+        # Iterate over 'sentence'.
+        for curr_char in sentence:
+            # Map each 'curr_char' to its index using its ASCII code.
+            mapped_index = ord(curr_char) - ord('a')
+
+            # 'curr_bit' represents the bit for 'curr_char'.
+            curr_bit = 1 << mapped_index
+
+            # Use 'OR' operation since we only add its bit for once.
+            seen |= curr_bit
+
+        # Once we finish iterating, check if 'seen' contains 26 bits of 1.
+        return seen == (1 << 26) - 1
+
+    def checkIfPangram_4(self, sentence: str) -> bool:
         # Array 'seen' of size 26.
         seen = [False] * 26
 
