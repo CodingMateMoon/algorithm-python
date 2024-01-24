66df2bc2f5688aa14d6f39562c5b0f0a2355130c
codingmatemoon <janghyun2478@gmail.com>
2024-01-25 00:10:18 +0900

    [leetcode] Missing Number [완료]

--- hashing/Missing Number.py
+++ hashing/Missing Number.py
@@ -3,7 +3,15 @@ from typing import List
 # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4602/
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return 2
+        is_exist = [False] * (len(nums) + 1)
+
+        for i in range(len(nums)):
+            is_exist[nums[i]] = True
+
+        for i in range(len(is_exist)):
+            if not is_exist[i]:
+                return i
+        return 0
 
 def test_missingNumber():
     """
