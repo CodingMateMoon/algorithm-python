6075b4a0c3938861e2722c134dea7d156e580f1d
codingmatemoon <janghyun2478@gmail.com>
2024-02-01 09:10:11 +0900

    [leetcode] Counting Elements [진행3]

--- hashing/Counting Elements.py
+++ hashing/Counting Elements.py
@@ -1,11 +1,16 @@
 from typing import List
 
-
+# https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4661/
 class Solution:
     def countElements(self, arr: List[int]) -> int:
 
-        for i in range(len(arr)):
-            
+        answer = 0
+
+        for i in range(len(arr) - 1):
+            if (arr[i] + 1) == arr[i + 1] or arr[i] == (arr[i + 1] + 1):
+                answer += 1
+        return answer
+
 
 
 def test_countElements():
@@ -18,3 +23,4 @@ def test_countElements():
     # 1,2 / 3,4
     assert solution.countElements([1, 2, 3]) == 2
     assert solution.countElements([1, 1, 3, 3, 5, 5, 7, 7]) == 0
+    assert solution.countElements([1,3,2,3,5,0]) == 3
