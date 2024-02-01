cefe81bb693b1040c187d7a643fbc5289c7e1e59
codingmatemoon <janghyun2478@gmail.com>
2024-02-02 08:31:59 +0900

    [leetcode] Counting Elements [진행4]

--- hashing/Counting Elements.py
+++ hashing/Counting Elements.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4661/
 class Solution:
     def countElements(self, arr: List[int]) -> int:
@@ -12,10 +13,10 @@ class Solution:
         return answer
 
 
-
 def test_countElements():
     """
     x, x+1 연속된 숫자쌍이 있는 경우 구하기
+    연속된 인덱스 상관없이 list안에만 존재하면 count. e.g) 1, 3, 2
     1 <= arr.length <= 1000
     0 <= arr[i] <= 1000
     """
@@ -23,4 +24,4 @@ def test_countElements():
     # 1,2 / 3,4
     assert solution.countElements([1, 2, 3]) == 2
     assert solution.countElements([1, 1, 3, 3, 5, 5, 7, 7]) == 0
-    assert solution.countElements([1,3,2,3,5,0]) == 3
+    assert solution.countElements([1, 3, 2, 3, 5, 0]) == 3
