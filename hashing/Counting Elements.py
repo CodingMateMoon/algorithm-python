a683c0a690ccab03f6abd1a6cb433f1759af04f6
codingmatemoon <abc9529@naver.com>
2024-02-08 16:33:16 +0900

    [leetcode] Counting Elements [보완1]

--- hashing/Counting Elements.py
+++ hashing/Counting Elements.py
@@ -4,6 +4,18 @@ from typing import List
 # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4661/
 class Solution:
     def countElements(self, arr: List[int]) -> int:
+        arr.sort()
+        count = 0
+        run_length = 1
+        for i in range(len(arr)):
+            if arr[i - 1] != arr[i]:
+                if arr[i - 1] + 1 == arr[i]:
+                    count += run_length
+                run_length = 0
+            run_length += 1
+            run_length += 1
+        return count
+    def countElements_2(self, arr: List[int]) -> int:
         hash_set = set(arr)
         count = 0
         for x in arr:
