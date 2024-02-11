bb1cc08e24ad83d86c9727df331107aa366d4088
codingmatemoon <abc9529@naver.com>
2024-02-11 17:33:18 +0900

    [leetcode] Perform String Shifts [진행2]

--- hashing/Perform String Shifts.py
+++ hashing/Perform String Shifts.py
@@ -2,6 +2,7 @@ class Solution(object):
     def stringShift(self, s, shift):
         """
         :type s: str
+        shift [direction, amount]
         :type shift: List[List[int]]
         :rtype: str
         """
@@ -9,4 +10,9 @@ class Solution(object):
 
 def test_stringShift():
     solution = Solution()
+    """
+    direction> 0 : left, 1 : right
+    amount> e.g) [0, 1]
+    [0,1] means shift to left by 1. "abc" -> "bca"
+    """
     assert solution.stringShift("abc", [[0, 1], [1,2]]) == "cab"
