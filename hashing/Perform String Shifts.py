bba36275c57b4824dece662cff46ee148a356eaa
codingmatemoon <janghyun2478@gmail.com>
2024-02-27 08:43:41 +0900

    [leetcode] Rotate Array [보완4]

--- hashing/Perform String Shifts.py
+++ hashing/Perform String Shifts.py
@@ -1,6 +1,22 @@
 # https://leetcode.com/problems/perform-string-shifts/description/
 class Solution(object):
     def stringShift(self, s, shift):
+        # Add up the left shifts and right shifts.
+        overall_shifts = [0, 0]
+        for direction, amount in shift:
+            overall_shifts[direction] += amount
+        left_shifts, right_shifts = overall_shifts
+
+        # Determine which shift (if any) to perform.
+        if left_shifts > right_shifts:
+            left_shifts = (left_shifts - right_shifts) % len(s)
+            s = s[left_shifts:] + s[:left_shifts]
+        else:
+            right_shifts = (right_shifts - left_shifts) % len(s)
+            s = s[-right_shifts:] + s[:-right_shifts]
+
+        return s
+    def stringShift_3(self, s, shift):
         for direction, amount in shift:
             amount %= len(s)
             if direction == 0:
