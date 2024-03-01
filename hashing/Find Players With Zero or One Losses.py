3a73c16e2799bbcd61438dd02089178e4da0b74b
codingmatemoon <janghyun2478@gmail.com>
2024-03-01 13:17:14 +0900

    [leetcode] Find Players With Zero or One Losses [진행2]

--- hashing/Find Players With Zero or One Losses.py
+++ hashing/Find Players With Zero or One Losses.py
@@ -1,6 +1,16 @@
 from typing import List
 
+#https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4606/
+"""
+Constraints:
 
+1 <= matches.length <= 105
+matches[i].length == 2
+1 <= winneri, loseri <= 105
+winneri != loseri
+
+matches[i] = [winner(i), loser(i)]
+"""
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         return [[1,2,10],[4,5,7,8]]
@@ -8,3 +18,4 @@ class Solution:
 def test_findWinners():
    solution = Solution()
    assert solution.findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]) == [[1,2,10],[4,5,7,8]]
+   assert solution.findWinners([[2,3],[1,3],[5,4],[6,4]]) == [[1,2,5,6],[]]
