5d73c832c3498883f114508a3b5734ad296f9047
codingmatemoon <janghyun2478@gmail.com>
2024-03-02 10:08:27 +0900

    [leetcode] Find Players With Zero or One Losses [진행3]

--- hashing/Find Players With Zero or One Losses.py
+++ hashing/Find Players With Zero or One Losses.py
@@ -17,5 +17,13 @@ class Solution:
 
 def test_findWinners():
    solution = Solution()
+   """
+   항상 이긴 경우, 1번만 진 경우 구하기.
+   
+    Players 1, 2, and 10 have not lost any matches.
+    Players 4, 5, 7, and 8 each have lost one match.
+    Players 3, 6, and 9 each have lost two matches.
+    Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+   """
    assert solution.findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]) == [[1,2,10],[4,5,7,8]]
    assert solution.findWinners([[2,3],[1,3],[5,4],[6,4]]) == [[1,2,5,6],[]]
