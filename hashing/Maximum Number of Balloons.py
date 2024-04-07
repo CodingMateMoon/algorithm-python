582639d2357ffbe586f6639644d44ed2c36ab6b9
codingmatemoon <janghyun2478@gmail.com>
2024-04-07 14:16:01 +0900

    [leetcode] Maximum Number of Balloons[진행2]

--- hashing/Maximum Number of Balloons.py
+++ hashing/Maximum Number of Balloons.py
@@ -1,7 +1,13 @@
 class Solution:
+    # https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4663/
     def maxNumberOfBalloons(self, text: str) -> int:
         return 1
 
 def test_maxNumberOfBalloons():
+    """
+    text에 있는 알파벳으로 balloon 최대 구성 가능한 횟수
+    :return:
+    """
     solution = Solution()
     assert solution.maxNumberOfBalloons("nlaebolko") == 1
+    assert solution.maxNumberOfBalloons("loonbalxballpoon") == 2
