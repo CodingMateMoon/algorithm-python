879f55fb2e6a69c331b622511bc9e35b785f4fe4
codingmatemoon <janghyun2478@gmail.com>
2024-04-26 08:17:13 +0900

    [leetcode] Contiguous Array(doing1)

---
+++ hashing/Contiguous Array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        return 2
+
+def test_findMaxLength():
+    solution = Solution()
+    assert solution.findMaxLength([0, 1]) == 2
+
