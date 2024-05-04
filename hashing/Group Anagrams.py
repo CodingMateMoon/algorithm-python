a9c3d429e8ade72f8a4112d07a5d3a57f661ba85
codingmatemoon <janghyun2478@gmail.com>
2024-05-04 11:37:01 +0900

    [leetcode] Group Anagrams(doing2)

--- hashing/Group Anagrams.py
+++ hashing/Group Anagrams.py
@@ -7,5 +7,8 @@ class Solution:
         return None
 
 def test_groupAnagrams():
+    """"
+    nat, tan 동일한 알파벳으로 구성된 문자열 그룹화. e, a, t로 구성된 문자열 탐색 및 그룹화. 
+    """
     solution = Solution()
     assert solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) == [["bat"],["nat","tan"],["ate","eat","tea"]]
