07be624170f230a70c70249e7519a104f53ec246
codingmatemoon <janghyun2478@gmail.com>
2024-05-19 09:20:09 +0900

    [leetcode] Minimum Consecutive Cards to Pick Up(doing2)

--- hashing/Minimum Consecutive Cards to Pick Up.py
+++ hashing/Minimum Consecutive Cards to Pick Up.py
@@ -6,5 +6,9 @@ class Solution:
         return 4
 
 def minimumCardPickup():
+    """
+    1 <= cards.length <= 105
+0 <= cards[i] <= 106
+    """
     solution = Solution()
     assert solution.minimumCardPickup([3,4,2,3,4,7]) == 4
