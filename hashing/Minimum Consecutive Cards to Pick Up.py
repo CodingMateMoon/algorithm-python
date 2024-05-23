b67563683d9ff64f332ead7613c47ecfba94c917
codingmatemoon <janghyun2478@gmail.com>
2024-05-23 09:29:57 +0900

    [leetcode] Minimum Consecutive Cards to Pick Up(doing6)

--- hashing/Minimum Consecutive Cards to Pick Up.py
+++ hashing/Minimum Consecutive Cards to Pick Up.py
@@ -14,6 +14,7 @@ def minimumCardPickup():
     0 <= cards[i] <= 106
  3, 4, 2, 3
  4, 2, 3, 4 등 값이 같은 숫자가 있는 최소 부분 집합
+ 요소별 숫자 개수 및 위치 관리. dict에 숫자별 count 관리하면서 count=2가 됐을 때 해당 숫자 첫번째 위치와 현재 위치를 통해 length 계산 후 최소값 갱신.
     """
     solution = Solution()
     assert solution.minimumCardPickup([3, 4, 2, 3, 4, 7]) == 4
