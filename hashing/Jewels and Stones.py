1de8871fa099a9369b1401c2958659a8bccbbcf9
codingmatemoon <janghyun2478@gmail.com>
2024-06-03 09:15:09 +0900

    [leetcode] Jewels and Stones (done)

--- hashing/Jewels and Stones.py
+++ hashing/Jewels and Stones.py
@@ -1,8 +1,17 @@
 #https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4664/
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return 3
+        answer = 0
+        for stone in stones:
+            if stone in jewels:
+                answer += 1
+        return answer
 
 def test_numJewelsInStones():
+    """
+    1 <= jewels.length, stones.length <= 50
+    jewels and stones consist of only English letters.
+    All the characters of jewels are unique.
+    """
     solution = Solution()
     assert solution.numJewelsInStones("aA", "aAAbbbb") == 3
