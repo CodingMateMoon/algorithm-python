39e4889d9197e511722a901014ce10e8420c08b1
codingmatemoon <janghyun2478@gmail.com>
2024-06-07 09:15:42 +0900

    [leetcode] Longest Substring Without Repeating Characters (doing2)

--- hashing/Longest Substring Without Repeating Characters.py
+++ hashing/Longest Substring Without Repeating Characters.py
@@ -1,8 +1,15 @@
+# https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4690/
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         return 3
 
 def test_lengthOfLongestSubstring():
     solution = Solution()
+    """
+    Given a string s, find the length of the longest substring without repeating characters.
+    0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+    """
+
     #The answer is "abc", with the length of 3.
     assert solution.lengthOfLongestSubstring("abcabcbb") == 3
