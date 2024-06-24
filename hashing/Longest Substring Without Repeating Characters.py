d896007a74bbf2ce8ff09850f6ec4e516ea443d1
codingmatemoon <abc9529@naver.com>
2024-06-24 16:14:57 +0900

    [leetcode] Longest Substring Without Repeating Characters[doing3]

--- hashing/Longest Substring Without Repeating Characters.py
+++ hashing/Longest Substring Without Repeating Characters.py
@@ -10,6 +10,14 @@ class Solution:
                 if i == len(s) - 1:
                     return max(max_length, len(before_element))
                 continue
+            for j in range(len(before_element)):
+                if s[i] == before_element[j]:
+                    for k in range(j):
+                        print(f"before pop:{before_element}")
+                        before_element.pop(k)
+                        print(f"after pop:{before_element}")
+                    before_element.append(s[i])
+
             max_length = max(max_length, len(before_element))
 
         return max_length
@@ -21,7 +29,7 @@ def test_lengthOfLongestSubstring():
     0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
     연속된 문자로 중복 없이 가장 긴 것 구하기. 현재 커서 기준으로 다음 문자가 다를 경우 length를 증가시키고 같은 문자가 나올 경우 max(length, max_length) 저장
-    dv 다음 d가 나오는 것처럼 이전 문자와는 다르나 그 앞에 문자가 중복해서 나오는 경우 초기화하지 않고 유지
+    dv 다음 d가 나오는 것처럼 이전 문자와는 다르나 그 앞에 문자가 중복해서 나오는 경우 초기화하지 않고 유지. 앞의 d를 pop 두번째부터 길이 계산. 
     """
 
     #The answer is "abc", with the length of 3.
