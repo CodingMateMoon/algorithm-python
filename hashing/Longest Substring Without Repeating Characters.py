3575e80382d3249e4440775b849485789fad995a
codingmatemoon <janghyun2478@gmail.com>
2024-07-11 09:01:40 +0900

    [leetcode] Longest Substring Without Repeating Characters (done)

--- hashing/Longest Substring Without Repeating Characters.py
+++ hashing/Longest Substring Without Repeating Characters.py
@@ -43,7 +43,7 @@ class Solution:
             for i in range(left, right):
                 print(f"{i}:{s[i]}")
         return result
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def lengthOfLongestSubstring_3(self, s: str) -> int:
         #char_index값을 저장 후 중복되는 char에 대해 해당 index 위치 다음부터 left 새로 시작.
         result = left = 0
         char_index = {}
@@ -56,6 +56,24 @@ class Solution:
 
         return result
 
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = [None] * 128
+
+        left = right = 0
+
+        res = 0
+        while right < len(s):
+            r = s[right]
+
+            index = chars[ord(r)]
+            if index is not None and left <= index < right:
+                left = index + 1
+
+            res = max(res, right - left + 1)
+
+            chars[ord(r)] = right
+            right += 1
+        return res
 
 
 def test_lengthOfLongestSubstring():
