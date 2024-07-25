18b05bdd2639718a24ff64fa3f10410ba13bfe3e
codingmatemoon <janghyun2478@gmail.com>
2024-07-26 08:49:38 +0900

    [leetcode] Linked List Cycle(doing15)

--- linked-list/Linked List Cycle.py
+++ linked-list/Linked List Cycle.py
@@ -24,6 +24,11 @@ class Solution:
         if head is None:
             return False
         slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            
 
 
 
