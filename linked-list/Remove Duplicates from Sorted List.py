f919ed5b8fcd6001eb03344c059d02fc443a4040
codingmatemoon <janghyun2478@gmail.com>
2024-08-13 08:41:00 +0900

    [leetcode] Remove Duplicates from Sorted List(doing5)

--- linked-list/Remove Duplicates from Sorted List.py
+++ linked-list/Remove Duplicates from Sorted List.py
@@ -17,11 +17,14 @@ Output: [1,2,3]
 The number of nodes in the list is in the range [0, 300].
 -100 <= Node.val <= 100
 The list is guaranteed to be sorted in ascending order.
+순회하면서 중복을 제거한 노드 인덱스 정보를 저장 후 next.next 등으로 건너뛰어서 저장. 이전 노드 정보 가지고 있기
 """
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        val_check = []
-        current_node = head
+        val_check = [head.val]
+        current_node = head.next
+        previous_node = head
         while current_node.next is not None:
             if current_node.val in val_check:
+                current_node.
                 
