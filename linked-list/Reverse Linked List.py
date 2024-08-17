d7069a5724aa428defcde06f6001c1a91af88f6b
codingmatemoon <janghyun2478@gmail.com>
2024-08-18 08:34:03 +0900

    [leetcode] Reverse Linked List(doing1)

--- linked-list/Reverse Linked List.py
+++ linked-list/Reverse Linked List.py
@@ -6,6 +6,12 @@ from typing import Optional
 """
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Input: head = [1,2]
+Output: [2,1]
+
 """
 class ListNode:
     def __init__(self, val=0, next=None):
