e1ca08bc15109d23b9b1e0bb3a3af29000bde1db
codingmatemoon <janghyun2478@gmail.com>
2024-09-03 09:25:17 +0900

    [leetcode] Swap Nodes in Pairs[done]

--- linked-list/Swap Nodes in Pairs.py
+++ linked-list/Swap Nodes in Pairs.py
@@ -58,6 +58,13 @@ class Solution:
             second_node = first_node.next
 
             prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            prev_node = first_node
+            head = first_node.next
+
+        return dummy.next
 
 
 def test_swapPairs():
