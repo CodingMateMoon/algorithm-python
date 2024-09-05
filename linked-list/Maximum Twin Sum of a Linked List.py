252e68ccd756fd858232f87124e0654d9c3421d6
codingmatemoon <janghyun2478@gmail.com>
2024-09-05 12:34:57 +0900

    [leetcode] Maximum Twin Sum of a Linked List [doing2]

--- linked-list/Maximum Twin Sum of a Linked List.py
+++ linked-list/Maximum Twin Sum of a Linked List.py
@@ -1,3 +1,4 @@
+#https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
 # Definition for singly-linked list.
 from typing import Optional
 
@@ -8,6 +9,13 @@ For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin
 The twin sum is defined as the sum of a node and its twin.
 
 Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+Input: head = [5,4,2,1]
+Output: 6
+Explanation:
+Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+There are no other nodes with twins in the linked list.
+Thus, the maximum twin sum of the linked list is 6.
 """
 class ListNode:
     def __init__(self, val=0, next=None):
