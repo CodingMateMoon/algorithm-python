bfb1b18c4d8e06fe50b09c23f6da4603c81d1b0f
codingmatemoon <janghyun2478@gmail.com>
2024-09-07 09:52:50 +0900

    [leetcode] Maximum Twin Sum of a Linked List [doing4]

--- linked-list/Maximum Twin Sum of a Linked List.py
+++ linked-list/Maximum Twin Sum of a Linked List.py
@@ -16,6 +16,21 @@ Explanation:
 Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
 There are no other nodes with twins in the linked list.
 Thus, the maximum twin sum of the linked list is 6.
+
+Ex2:
+Input: head = [4,2,2,3]
+Output: 7
+Explanation:
+The nodes with twins present in this linked list are:
+- Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+- Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+Thus, the maximum twin sum of the linked list is max(7, 4) = 7. 
+
+Constraints:
+
+The number of nodes in the list is an even integer in the range [2, 105].
+1 <= Node.val <= 105
+
 index 합이 n-1. n=4 -> 0 + 3 = 3, 1 + 2 = 3 [n-1]
 twin 노드 value 합이 가장 큰 경우 구하기
 """
@@ -25,3 +40,4 @@ class ListNode:
         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+
