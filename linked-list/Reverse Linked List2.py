328574784c695c13df8b1da44fe7ad7c145861a7
codingmatemoon <janghyun2478@gmail.com>
2024-10-17 13:45:41 +0900

    [leetcode] Reverse Linked List2 [doing1]

--- linked-list/Reverse Linked List2.py
+++ linked-list/Reverse Linked List2.py
@@ -83,30 +83,6 @@ class Solution:
         recurseAndReverse(right_h, left, right)
         return head
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        cur, prev = head, None
-        while left > 1:
-            prev = cur
-            cur = cur.next
-            left, right = left - 1, right - 1
-
-        tail, con = cur, prev
-
-        while right:
-            third = cur.next
-            cur.next = prev
-            prev = cur
-            cur = third
-            right -= 1
-
-        if con:
-            con.next = prev
-        else:
-            head = prev
-        tail.next = cur
-        return head
 
 
 
