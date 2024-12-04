8b04c6b153ca1f4b44a2bc2fba01f76b6d3df41f
codingmatemoon <janghyun2478@gmail.com>
2024-12-04 16:58:59 +0900

    [leetcode] Delete the Middle Node of a Linked List [done]

--- linked-list/Delete the Middle Node of a Linked List.py
+++ linked-list/Delete the Middle Node of a Linked List.py
@@ -85,6 +85,9 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
 
+        slow.next = slow.next.next
+        return head
+
     def custom_print(self):
         count = 7
         middle_index = math.floor(count / 2)
